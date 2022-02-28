8195798540b3907f1e6da95374e58aaca21895ff
laithml <laithml95@gmail.com>
2022-02-28 18:13:16 +0200

    Final version with readme file

---
+++ Readme.txt
@@ -0,0 +1,44 @@
+String parser
+Authored by Laith Al-mouhtaseb
+211353297
+
+==Description==
+The program start in infinite loop that stopped when the user enter "exit" to the input,
+The program get a string input from the user and count how many words and letters in the words
+after that save the input string in a text file I named "history.txt",
+and if the user entered "history",the program print all the strings that the user have entered it.
+
+functions:
+history: its void function with no input args, that open the txt file "history.txt" and print the file to the console.
+
+count: the parameters that require it is  string, pointer of int and another pointer of int, and return a string,
+the function edit the value of the two ints that were given to it,it takes ints by reference.
+one int to count how many words in the string,
+and the second one to count how many letters in the string,
+the string that given to the function is the string that the user give in the main.
+
+how it works: define a new string with length of 8 because the longest word that wil return is "history" and the '\0' at the end of it.
+it runs throw the string and if the char at index i isn't space " " so it's a legal char so the letter counter++.
+if the char at index i is space " " and there's 1 or more legal chars so the word count++ and at the end if the char counter is larger than 1.
+add 1 for the word counter, because the word count actually count the splits between words.
+at the end the function check if the first word equal "exit" or "history" and return what is if its else return ""
+and in main they check if the returned value of count function equal "exit" or "history" and the wordcount equal to 1 so the main do what the word have to do.
+and if it equals to "" so that a normal input, and we have to add it to history file.
+
+
+countLine: function that run throw the file and count how many rows in the history file.
+
+==Program Files==
+main.c
+
+==How to compile==
+compile: gcc main.c -o main
+run: ./main
+
+==input==
+string from user
+
+==output==
+history.txt file
+words count for every input
+chars count for every input
--- main.c
+++ main.c
@@ -8,41 +8,46 @@ const char *count(char[], int *, int *);
 int countLine();
 
 int main() {
-    char str[510];
+    char str[512];//I added 2 to the length because there's '\n\0' at the end of the str
     FILE *writeFile;
     writeFile = fopen("history.txt", "a");
-    int i = countLine();
-    while (-1) {
-        int charCount = 0, wordCount = 0;
-        printf("Enter string, or \"exit\" to end program:\n");
-        fgets(str, 510, stdin);
-        str[strlen(str) - 1] = '\0';
-        if (strcmp(str, "exit") == 0) break;
-        else if (strcmp(str, "history") == 0) {
-            fclose(writeFile);
-            history();
-            writeFile = fopen("history.txt", "a");
-        } else {
-            const char *word = count(str, &charCount, &wordCount);
-            if (wordCount != 0) {
-                if (wordCount == 1 && strcmp(word, "exit") == 0)
-                    break;
-                else if (wordCount == 1 && strcmp(word, "history") == 0) {
-                    fclose(writeFile);
-                    history();
-                    writeFile = fopen("history.txt", "a");
-                } else {
-                    printf("%d words\n%d chars\n", wordCount, charCount);
-                    fprintf(writeFile, "%d: %s\n", i, str);
-                    i++;
+    if(writeFile==NULL){
+        perror("cant create a new file");
+        return 1;
+    }else {
+        int i = countLine();
+        while (-1) {
+            int charCount = 0, wordCount = 0;
+            printf("Enter string, or \"exit\" to end program:\n");
+            fgets(str, 510, stdin);
+            str[strlen(str) - 1] = '\0';    //because the user press enter so '\n' enter to the input string in the last index, so we put '\0'
+            if (strcmp(str, "exit") == 0) break;
+            else if (strcmp(str, "history") == 0) {
+                fclose(writeFile);//close the file to save every input before "history" to the file, to can read it in the history function
+                history();
+                writeFile = fopen("history.txt", "a");//open the file again to continue to append new input in it
+            } else {
+                const char *word = count(str, &charCount, &wordCount);
+                if (wordCount != 0) {
+                    if (wordCount == 1 && strcmp(word, "exit") == 0) // if their only one word, so I have to check if it "exit" or "history"
+                        break;
+                    else if (wordCount == 1 && strcmp(word, "history") == 0) {
+                        fclose(writeFile);
+                        history();
+                        writeFile = fopen("history.txt", "a");
+                    } else {
+                        printf("%d words\n%d chars\n", wordCount, charCount);
+                        fprintf(writeFile, "%d: %s\n", i, str);
+                        i++;
+                    }
                 }
             }
         }
+
+        fclose(writeFile);
+
+        return 0;
     }
-
-    fclose(writeFile);
-
-    return 0;
 }
 
 void history() {
@@ -51,7 +56,7 @@ void history() {
     if (readFile == NULL) {
         perror("file doesn't exist");
     } else {
-        char c = fgetc(readFile);;
+        char c = fgetc(readFile);//read character by character and print it to the
         while (c != EOF) {
             printf("%c", c);
             c = fgetc(readFile);
@@ -61,21 +66,22 @@ void history() {
 }
 
 int countLine() {
+    //count how many lines in the file
+    int counter = 0;
     FILE *readFile;
     readFile = fopen("history.txt", "r");
     if (readFile == NULL) {
         perror("file doesn't exist");
     } else {
-        int counter = 0;
-        char c = fgetc(readFile);;
+        char c = fgetc(readFile);
         while (c != EOF) {
             if (c == '\n') counter++;
             c = fgetc(readFile);
 
         }
         fclose(readFile);
-        return counter;
     }
+    return counter;
 }
 
 const char* count(char str[], int *charCount, int *wordCount) {
@@ -85,24 +91,24 @@ const char* count(char str[], int *charCount, int *wordCount) {
     int wordInd = 0;
 
     while (i < len) {
-        if (str[i] != ' ') {
+        if (str[i] != ' ') {//if the char is not ' ' so we have to count iy
             (*charCount)++;
-            if ((*wordCount) < 2 && (*charCount) < 8){
+            if ((*wordCount) < 1 && (*charCount) < 8){ // if we are in the first word we enter the first 8 chars to the word to check if it "exit" or "history"
                 word[wordInd] = str[i];
                 wordInd++;
             }
 
-        } else if ((*charCount) != 0 && str[i + 1] != ' ' && str[i + 1] != '\0')
+        } else if ((*charCount) != 0 && str[i + 1] != ' ' && str[i + 1] != '\0')//if the str[i] is ' ' and the next element isn't ' ' or '\0' so it's an legal char for a new word
             (*wordCount)++;
         i++;
     }
-    if ((*charCount) > 0)
+    if ((*charCount) > 0)//because we count the splits so the number of splits is more than 0, so there is (1+splits) words
         (*wordCount)++;
-    word[wordInd]='\0';
-    if(strcmp(word,"exit")==0)
+    word[wordInd]='\0';// to finish the string if it less than 7 chars like "exit"
+    if(strcmp(word,"exit")==0)// if the first word in the input is "exit" return  "exit"
         return "exit";
-    if(strcmp(word,"history")==0)
+    if(strcmp(word,"history")==0)// if the first word in the input is "history" return  "history"
         return "history";
-
+//else return ""
     return "";
 }
